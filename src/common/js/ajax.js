857d7a4ae35a9fdb873d791851b6a3b4da4871ab
cggcbb <449567533@qq.com>
2019-04-27 00:20:00 +0800

    update ajax

--- src/common/js/ajax.js
+++ src/common/js/ajax.js
@@ -5,6 +5,24 @@ import { Modal } from 'antd'
 const STATUS_SUCCESS = 200
 const CODE_SUCCESS = 0
 
+const instance = axios.create({
+  baseURL: 'https://www.easy-mock.com/mock/5cabf67f1a2ff67cf5210f83/manager/mock',
+  timeout: 5000
+})
+
+// 发送ajax请求之前
+instance.interceptors.request.use((config) => {
+  return config
+}, error => {
+  return Promise.reject(error)
+})
+// 接收到数据, 处理数据之前
+instance.interceptors.response.use((res) => {
+  return res
+}, error => {
+  return Promise.reject(error)
+})
+
 export const ajax = (options) => {
   let loading
   let { isShowLoading = true } = options
@@ -12,13 +30,10 @@ export const ajax = (options) => {
     loading = document.getElementById('loadingWrapper')
     loading.style.display = 'block'
   }
-  const baseURL = 'https://www.easy-mock.com/mock/5cabf67f1a2ff67cf5210f83/manager/mock'
   return new Promise((resolve, reject) => {
-    axios({
+    instance({
       url: options.url,
       method: options.method || 'get',
-      baseURL,
-      timeout: 5000,
       params: options.params || ''
     }).then(res => {
       if (loading) {
