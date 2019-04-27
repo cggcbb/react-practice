36c0500a97ab982e012356de9af27164423c271f
cggcbb <449567533@qq.com>
2019-04-27 18:56:33 +0800

    add updateToken

--- src/redux/action/action.jsx
+++ src/redux/action/action.jsx
@@ -1,10 +1,18 @@
 export const types = {
-  SWITCH_MENU: 'SWITCH_MENU'
+  SWITCH_MENU: 'SWITCH_MENU',
+  UPDATE_TOKEN: 'UPDATE_TOKEN'
 }
 
 export const switchMenu = (menuName) => {
   return {
     type: types.SWITCH_MENU,
     menuName
+  }
+}
+
+export const updateToken = (token) => {
+  return {
+    type: types.UPDATE_TOKEN,
+    token
   }
 }
--- src/redux/reducer/reducer.jsx
+++ src/redux/reducer/reducer.jsx
@@ -1,7 +1,8 @@
 import { types } from '../action/action'
 
 const initialState = {
-  menuName: '首页'
+  menuName: '首页',
+  token: null
 }
 
 export default (state = initialState, action) => {
@@ -10,6 +11,11 @@ export default (state = initialState, action) => {
       return {
         ...state,
         menuName: action.menuName
+      }
+    case types.UPDATE_TOKEN:
+      return {
+        ...state,
+        token: action.token
       }
     default:
       return {
