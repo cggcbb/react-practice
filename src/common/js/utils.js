b1d0df7b4ffa4cf308e12f116b414eb4f853ee5f
cggcbb <449567533@qq.com>
2019-05-06 22:21:56 +0800

    commit utils ->  pagination

--- src/common/js/utils.js
+++ src/common/js/utils.js
@@ -10,6 +10,37 @@ const optimizeCurrentTime = () => {
   return `${current.getFullYear()}-${date} ${time}`
 }
 
+// page分页
+const pagination = (data, options) => {
+  const {
+    onChange, 
+    showQuickJumper,
+    pageSizeOptions,
+    hideOnSinglePage,
+    onShowSizeChange
+  } = options
+  
+  return {
+    current: data.pageNo,
+    pageSize: data.pageSize,
+    total: data.totalCount,
+    pageSizeOptions,
+    showSizeChanger: pageSizeOptions ? true : false,
+    onChange: onChange ? (current) => {
+      onChange(current)
+    } : null,
+    showTotal: () => {
+      return `共${data.totalCount}条, ${Math.ceil(data.totalCount / data.pageSize)}页`
+    },
+    onShowSizeChange: onShowSizeChange ? (current, size) => {
+      onShowSizeChange(current, size)
+    } : null,
+    showQuickJumper: showQuickJumper ? true : false,
+    hideOnSinglePage: hideOnSinglePage ? true : false
+  }
+}
+
 export {
-  optimizeCurrentTime
+  optimizeCurrentTime,
+  pagination
 }
